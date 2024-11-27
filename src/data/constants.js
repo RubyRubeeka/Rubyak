9ea22f4b00e66a8da3896d4a30e7fc58fd3c25a5
RubyRubeeka <rubyrubeeka09@gmail.com>
2024-11-27 20:34:29 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,119 @@
+export const Bio = {
+  name: "Rubeeka",
+  roles: [
+    "Front End Developer",
+    "Web Developer",
+    "UI/UX Designer",
+    "Editor",
+    
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  resume:
+    "https://drive.google.com/file/d/1GMbgRRK795oMbzDnRNipV1QURePEiUrp/view?usp=drivesdk",
+  linkedin: "https://www.linkedin.com/in/ruby-rubeeka-3225052a5/",
+  insta: "https://www.instagram.com/sassy_x_ruby/",
+
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+     },
+    
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+
+        },
+      
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      
+    ],
+  },
+  
+    
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+
+export const education = [
+  {
+    id: 0,
+    school: "University of Madras - Sri Sankara Arts And Science College, Kanchipuram.",
+    date:  "BCA 2022 - 2025",
+    grade: "8.41 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Application at Sri Sankara Arts And Science College, Kanchipuram. I have completed 6 semesters and have a CGPA of 8.71. I have taken Diploma courses in Computer Hardware and Networking.",
+    degree: "[BCA] Bachelor of Computer Application",
+  },
+  {
+    id: 1,
+   img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+    school: "Arcot Narayanasamy Mudaliyar Govt.Girls.Hr.Sec School, Kanchipuram.",
+    date: "HSC-2022",
+    grade: "53%",
+    desc: "I completed my class 12 high school education at Kanchipuram, where I studied Maths with Computer Science.",
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+    school: "BM Misrilal Sowkar Govt.Girls.Hr.Sec School, Kanchipuram",
+    date: "SSLC-2020",
+    grade: "60%",
+    desc: "I completed my class 10 education at BM Misrilal Sowkar Hr.Sec School, Kanchipuram.",
+ },
+];
+
